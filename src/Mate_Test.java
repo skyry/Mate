7bc4ef0f132cad63d138035ea240b5c2d4f48add
skyry <33283945+skyry@users.noreply.github.com>
2021-10-24 11:24:33 +0300

    Initial commit

---
+++ src/Mate_Test.java
@@ -0,0 +1,18 @@
+public class Mate_Test {
+    public static void main(String[] args) {
+        /*int startNumber = 35;
+        int endNumber = 42;
+        for (int i = startNumber; i <= endNumber; i++) {
+            if (i % 5 == 0 && i % 7 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("fizz");
+            } else if (i % 7 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);}
+        } */
+
+    }
+
+}
